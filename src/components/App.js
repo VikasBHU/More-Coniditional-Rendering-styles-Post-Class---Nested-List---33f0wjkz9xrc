a41164daca9dc0eca19214a0da4a3266b7f45f63
Vikas Pandey <96241726+VikasBHU@users.noreply.github.com>
2022-04-03 00:26:10 +0530

    More Coniditional Rendering & styles [Post Class] - Nested List

--- src/components/App.js
+++ src/components/App.js
@@ -1,161 +1,65 @@
 import React, { Component, useState } from "react";
-import "./../styles/App.css";
+import "../styles/App.css";
 
-// Do not alter the states const and values inside it.
-const states = [
-  {
-    name: "Madhya Pradesh",
-    cities: [
-      {
-        name: "Indore",
-        towns: [
-          {
-            name: "Mhow",
-          },
-          {
-            name: "Dewas",
-          },
-        ],
-      },
-      {
-        name: "Bhopal",
-        towns: [
-          {
-            name: "Manit",
-          },
-          {
-            name: "Berasia",
-          },
-        ],
-      },
-      {
-        name: "Gwalior",
-        towns: [
-          {
-            name: "Ajaypur",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Jharkhand",
-    cities: [
-      {
-        name: "Dhanbad",
-        towns: [
-          {
-            name: "IIT(ISM) Dhanbad",
-          },
-          {
-            name: "Hirapur",
-          },
-        ],
-      },
-      {
-        name: "Wasseypur",
-        towns: [
-          {
-            name: "Sardar khan's",
-          },
-          {
-            name: "Faizal khan's",
-          },
-        ],
-      },
-      {
-        name: "Mirzapur",
-        towns: [
-          {
-            name: "Kaleen bhaiya's",
-          },
-          {
-            name: "Guddu bhaiya's",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Assam",
-    cities: [
-      {
-        name: "Guwhati",
-        towns: [
-          {
-            name: "Amin",
-          },
-          {
-            name: "Jalah",
-          },
-        ],
-      },
-      {
-        name: "Jungle1",
-        towns: [
-          {
-            name: "Tiger found at IIT Guwahati",
-          },
-          {
-            name: "Leopard found in IIT Guwahati",
-          },
-        ],
-      },
-      {
-        name: "Tezpur",
-        towns: [
-          {
-            name: "Dibrugarh",
-          },
-          {
-            name: "Silchar",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Bihar",
-    cities: [
-      {
-        name: "Patna",
-        towns: [
-          {
-            name: "Sonpur",
-          },
-          {
-            name: "Maner",
-          },
-        ],
-      },
-      {
-        name: "Gaya",
-        towns: [
-          {
-            name: "Bakraur",
-          },
-          {
-            name: "Barachatti",
-          },
-        ],
-      },
-      {
-        name: "Darbhanga",
-        towns: [
-          {
-            name: "Singhwara",
-          },
-          {
-            name: "Jale",
-          },
-        ],
-      },
-    ],
-  },
-];
-
-function App() {
-  return <div id="main"></div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        {
+          name: "naman",
+          percent: 60.0
+        },
+        {
+          name: "ritesh",
+          percent: 99.9431
+        },
+        {
+          name: "hitesh",
+          percent: 65.3365
+        },
+        {
+          name: "shreya",
+          percent: 80.64
+        },
+        {
+          name: "ankur",
+          percent: 78.897
+        },
+        {
+          name: "vanshu",
+          percent: 78.9876
+        },
+        {
+          name: "krupal",
+          percent: 85.3214
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div id="main">
+        {
+          //Correct the percentage conditional for correct output
+          this.state.list.map(function (item, index) {
+            if (item.percent > 0) {
+              return (
+                //Conditional statement in the className of below div to give callName="bg-pink"
+                //if the percent >= 90 else className = ""
+                <div key={index} className={}>
+                  <div className="name">//Write code to display name</div>
+                  <div className="percent">
+                    //Write code to display percentage upto 2 decimal places
+                  </div>
+                </div>
+              );
+            }
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
